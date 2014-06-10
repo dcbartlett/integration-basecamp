6d737a7f5143e60177e7c3f58fd2f7472f6dfecd
Dennis Bartlett <bartlett.dc.1@gmail.com>
2014-06-09 20:17:12 -0500

    Advancing helloworld

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ VERSION = '0.1'
 
 setup(
 	name=PACKAGE,
-    version=VERSION,
-    packages=['helloworld'],
-    entry_points={'trac.plugins': '%s = helloworld' % PACKAGE},
-    package_data={'helloworld': ['templates/*.html']},
+	version=VERSION,
+	packages=['helloworld'],
+	entry_points={'trac.plugins': '%s = helloworld' % PACKAGE},
+	package_data={'helloworld': ['templates/*.html']},
 )
