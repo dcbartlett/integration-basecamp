fad9ae7bf8c0fc8d2cc157455f4462a8c6e43434
Dennis Bartlett <bartlett.dc.1@gmail.com>
2014-06-09 20:33:39 -0500

    Advancing helloworld

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
-PACKAGE = 'TracHelloworld'
-VERSION = '0.1'
-
+# name can be any name.  This name will be used to create the .egg file.
+# name that is used in packages is the one that is used in the trac.ini file.
+# use package name as entry_points
 setup(
-	name=PACKAGE,
-	version=VERSION,
-	packages=['helloworld'],
-	entry_points={'trac.plugins': '%s = helloworld' % PACKAGE},
-	package_data={'helloworld': ['templates/*.html']},
+    name='Trachelloworld', version='1.1',
+    packages=find_packages(exclude=['*.tests*']),
+    entry_points = """
+        [trac.plugins]
+        helloworld = helloworld
+    """,
+    package_data={'helloworld': ['templates/*.html']},
 )
